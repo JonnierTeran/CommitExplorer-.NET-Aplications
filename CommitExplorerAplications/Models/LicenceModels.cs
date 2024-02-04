7003721fb29ca8660117acd2e5a38d94d5644249
Jonnier Teran <jonnierteranmorales@gmail.com>
2024-02-04 11:35:22 -0500

    Creando Modelo para Licence del repositorio

---
+++ CommitExplorerAplications/Models/LicenceModels.cs
@@ -0,0 +1,14 @@
+﻿namespace CommitExplorerAplications.Models
+{
+    /// <summary>
+    /// Modelo para Mappear Cada Licencia de Un Repositorio
+    /// </summary>
+    public class LicenceModels
+    {
+        public string key { get; set; } = string.Empty;
+        public string name { get; set; } = string.Empty;
+        public string spdx_id { get; set; } = string.Empty;
+        public string url { get; set; } = string.Empty;
+        public string node_id { get; set; } = string.Empty;
+    }
+}
