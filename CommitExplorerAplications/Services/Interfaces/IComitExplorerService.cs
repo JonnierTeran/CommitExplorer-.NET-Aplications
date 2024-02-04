ca11bfbcf2c702dcf4cdc3336127bbdffbbc61c3
Jonnier Teran <jonnierteranmorales@gmail.com>
2024-02-04 11:55:43 -0500

    Modificando Interface para el servicio

--- CommitExplorerAplications/Services/Interfaces/IComitExplorerService.cs
+++ CommitExplorerAplications/Services/Interfaces/IComitExplorerService.cs
@@ -8,6 +8,11 @@ namespace CommitExplorerAplications.Services.Interfaces
     /// </summary>
     public interface IComitExplorerService
     {
+        /// <summary>
+        /// Metodo Expuesto Para el Controlador
+        /// </summary>
+        /// <param name="request"> Recibe Un Objeto de Tipo RequestCommits con la Libreria y cantidad de repositorios deseados</param>
+        /// <returns> Regresa un Arrays de Objetos de tipo ResponseCommit con la informacion de los repositorios</returns>
         Task<IActionResult> CommitsPorSemana(RequestCommits request);
     }
 }
