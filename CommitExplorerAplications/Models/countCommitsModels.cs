14ca9649e6c191aa298ff33a06316d5d3b456659
Jonnier Teran <jonnierteranmorales@gmail.com>
2024-02-04 11:31:33 -0500

    Creando Modelo para mapear la cantidad de commits por semana de un repositorio

---
+++ CommitExplorerAplications/Models/countCommitsModels.cs
@@ -0,0 +1,28 @@
+﻿namespace CommitExplorerAplications.Models
+{
+    /// <summary>
+    /// Modelo para Mapear Todos los commits por semana de un repositorio
+    /// </summary>
+    public class countCommitsModels
+    {
+        /// <summary>
+        /// ES: Todos las confirmaciones por semana Por todos los colaboradores y autor
+        /// </summary>
+        public List<int>? All { get; set; }
+
+        /// <summary>
+        /// Todas las confirmaciones por El Autor
+        /// </summary>
+        public List<int>? Owner { get; set; }
+
+        /// <summary>
+        /// Constructor de la Clase
+        /// </summary>
+        public countCommitsModels()
+        {
+            this.All = new List<int>();
+            this.Owner = new List<int>();
+        }
+
+    }
+}
