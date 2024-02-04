1f16003ad63596b1a79a764b92cc399de507e6d2
Jonnier Teran <jonnierteranmorales@gmail.com>
2024-02-04 11:53:18 -0500

    Agregando Interface para el Servicio Y exponiendo el metodo que sera publico para ser usado en el controlador del API

---
+++ CommitExplorerAplications/Services/Interfaces/IComitExplorerService.cs
@@ -0,0 +1,13 @@
+﻿using CommitExplorerAplications.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommitExplorerAplications.Services.Interfaces
+{
+    /// <summary>
+    /// Metodo que exponemos al Controlador
+    /// </summary>
+    public interface IComitExplorerService
+    {
+        Task<IActionResult> CommitsPorSemana(RequestCommits request);
+    }
+}
